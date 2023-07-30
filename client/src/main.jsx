e0fea31c8b621d0ee4f2eb66d2d020ca97bf79cb
Jadesola Adeagbo <93772332+jadesolaadeagbo@users.noreply.github.com>
2023-07-30 20:39:51 +0100

    Frontend

---
+++ client/src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import "./index.css";
+import { TransactionsProvider } from "./context/TransactionContext";
+
+ReactDOM.render(
+  <TransactionsProvider>
+
+      <App/>
+
+  </TransactionsProvider>,
+
+
+  document.getElementById("root"),
+);
---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from "./Transactions.json";
+
+
+export const contractABI = abi.abi
+export const contractAddress = "0xF54C753F7d937Fd4D785F0a6DEB38a5b2D294E9f"
